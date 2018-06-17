878230ea26dc6ca3cc9d71559b330f0521d4e06e
SoumyaRajan <soumyarajan1994@gmail.com>
2018-06-17 12:53:41 +0530

    all files

---
+++ src/test/java/sampletGitTest/SampleGitTest.java
@@ -0,0 +1,30 @@
+package sampletGitTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+//import com.steadystate.css.util.Output;
+
+public class SampleGitTest {
+
+        @Test
+        public void logintest() throws IOException
+        {
+                WebDriver wd = new FirefoxDriver();
+                wd.manage().window().maximize();
+                wd.get("https://www.bing.com");
+                File src=((TakesScreenshot) wd).getScreenshotAs(OutputType.FILE);
+                FileUtils.copyFile(src, new File("C:\\workspace1\\Samplegittest\\screenshot\\test.jpg"));
+
+        }
+
+}
+
