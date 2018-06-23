16e92876d5b3b79eb5410fcd70dab849065accab
SoumyaRajan <soumyarajan1994@gmail.com>
2018-06-23 11:50:48 +0530

    all files updated

--- src/test/java/sampletGitTest/SampleGitTest2.java
+++ src/test/java/sampletGitTest/SampleGitTest2.java
@@ -13,16 +13,16 @@ import org.testng.annotations.Test;
 
 //import com.steadystate.css.util.Output;
 
-public class SampleGitTest {
+public class SampleGitTest2 {
 
         @Test
         public void logintest() throws IOException, InterruptedException
         {
                 WebDriver wd = new FirefoxDriver();
                 wd.manage().window().maximize();
-                wd.get("https://www.bing.com");
+                wd.get("https://www.ntltaxi.com");
                 File src=((TakesScreenshot) wd).getScreenshotAs(OutputType.FILE);
-                FileUtils.copyFile(src, new File("C:\\workspace1\\Samplegittest\\screenshot\\test.jpg"));
+                FileUtils.copyFile(src, new File("C:\\workspace1\\Samplegittest\\screenshot\\test1.jpg"));
                 Thread.sleep(3000);
         }
 
